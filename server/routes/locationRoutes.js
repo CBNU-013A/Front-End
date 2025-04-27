253c807917554f7f18d485aaa41e0c6034f043c5
lemongza <92498054+lemongza@users.noreply.github.com>
2025-04-27 16:05:03 +0900

    폴더 정리

--- server/routes/locationRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const locationController = require("../controller/locationController");
-router.post("/location", locationController.getLocationWithKeywords);
-router.get("/location", locationController.getLocationWithKeywords);
-
-module.exports = router;
